069c3d04d6524fbad7f342c2fe228498ebbbeeaf
Leonardo <leonardo_arrieta@solutionbox.com.ar>
2023-06-12 13:56:42 -0300

    deplo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
-import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import lightTheme from "./themes/light-theme";
-import darkTheme from "./themes/dark-theme";
-import { UIProvider } from "../context/ui/UIProvider";
-import { EntriesProvider } from "../context/entries/EntriesProvider";
 
-function MyApp({ Component, pageProps }: AppProps) {
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import React, { ReactElement } from "react";
+
+import type { AppProps } from "next/app";
+import { EntriesProvider } from "../context/entries/EntriesProvider";
+import { UIProvider } from "../context/ui/UIProvider";
+import darkTheme from "./themes/dark-theme";
+import lightTheme from "./themes/light-theme";
+
+function MyApp({ Component, pageProps }: AppProps): ReactElement {
   return (
     <EntriesProvider>
       <UIProvider>
