134ea74f8a045bd0e757319f7e7d00634affd3b4
Leonardo <leonardo_arrieta@solutionbox.com.ar>
2023-06-13 12:05:13 -0300

    deploy

--- next.config.js
+++ next.config.js
@@ -2,31 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack: (config, { dev, isServer }) => {
-    // Configuración de Webpack
-    config.optimization.minimize = true; // Minimizar el código
-    config.optimization.minimizer[0].options.terserOptions = {
-      compress: {
-        drop_console: true, // Eliminar console.log() en producción
-      },
-    };
-
-    // Configuración específica del servidor
-    if (isServer && !dev) {
-      // Configurar opciones de minimización para el servidor
-      config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true;
-    }
-
-    // Habilitar Webpack 5
-    config.experiments = {
-      topLevelAwait: true, // Habilitar el uso de import() para la carga de módulos
-    };
-    config.resolve.fallback = {
-      assert: false, // Deshabilitar el polyfill de assert en Webpack 5
-    };
-
-    return config;
-  },
+  
 }
 
 module.exports = nextConfig
