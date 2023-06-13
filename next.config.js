09b1b5b0b1c2ff2b14286b4d265772f028b83729
Leonardo <leonardo_arrieta@solutionbox.com.ar>
2023-06-13 12:00:21 -0300

    next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,36 @@
-/** @type {import('next').NextConfig} */
+/* @type {import('next').NextConfig} 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = nextConfig */
+module.exports = {
+  webpack: (config, { dev, isServer }) => {
+    // Optimizaciones de Webpack
+    config.optimization.minimize = true;
+    config.optimization.minimizer[0].options.terserOptions = {
+      // Opciones de Terser para minificar el código
+      compress: {
+        drop_console: true, // Eliminar console.log() en producción
+      },
+    };
+
+    // Configuración específica del servidor
+    if (isServer) {
+      // Eliminar polyfills innecesarios en el servidor
+      const originalEntry = config.entry;
+      config.entry = async () => {
+        const entries = await originalEntry();
+        if (entries['main.js'] && !dev) {
+          entries['main.js'].unshift('core-js/modules/es.promise');
+          entries['main.js'].unshift('core-js/modules/es.array.iterator');
+        }
+        return entries;
+      };
+    }
+
+    return config;
+  },
+};
+
